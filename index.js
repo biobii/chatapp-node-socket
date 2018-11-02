e77f1482dc8825a8d214ba38a4e79322646e32bf
Mohammad Robih T. Z <biobii.game@gmail.com>
2018-11-02 22:25:16 +0700

    public chat

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+var clients = [];
+
+app.use(express.static(__dirname + '/assets'))
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/views/login.html`)
+})
+
+app.get('/room', (req, res) => {
+  res.sendFile(`${__dirname}/views/chat.html`)
+})
+
+io.on('connection', socket => {
+  socket.on('userJoin', user => {
+      socket.id = user.name
+      clients.push({ ID: user.ID, name: user.name })
+      io.emit('userJoin', clients)
+      console.log(`${socket.id} bergabung`)
+  })
+
+  socket.on('chat', chat => {
+    io.emit('chat', chat)
+  })
+
+  socket.on('disconnect', () => {
+    if (!socket.id) return
+
+    clients.splice(clients.findIndex(c => c.name === socket.id), 1)
+    io.emit('userJoin', clients)
+    io.emit('disconnect', clients)
+  })
+})
+
+http.listen(process.env.PORT || 3000, '192.168.1.14' || 'localhost', () => console.log('SERVER RUNNING!'))
